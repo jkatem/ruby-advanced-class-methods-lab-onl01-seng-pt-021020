b680df540dc7f9b1e5b3571ed6a05acc6e0b42a9
Jen Miao <jenmiao@gmail.com>
2020-03-03 08:15:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,26 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
-  @@all = []
 
-  def self.all
+    # def name= (x)
+    #   @name = x
+    # end
+
+    # def name
+    #   @name
+    # end
+
+
+  @@all = []   # => This is a class variable with an empty array
+
+  def self.all  # => basically saying Song.all
+    #do something
     @@all
   end
 
+binding 'pry'
+ 
   def save
     self.class.all << self
   end
